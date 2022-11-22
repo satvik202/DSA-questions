532b8827f7a8c58c9c7cfd5345c38bfb32665f76
satvik <7vikpandey@gmail.com>
2022-11-22 20:53:56 +0530

    added program to find the unique element

---
+++ problems/array/unique_element.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int findUnique(int *arr, int n) {
+    // Write your code here
+    int ans;
+    for(int i=0;i<n;i++){
+        ans=ans^arr[i];
+    }
+    return ans;
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int size;
+        cin >> size;
+        int *input = new int[size];
+
+        for (int i = 0; i < size; ++i) {
+            cin >> input[i];
+        }
+
+        cout << findUnique(input, size) << endl;
+    }
+
+    return 0;
+}
